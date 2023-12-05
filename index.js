a46638660cb2db45b6e3254e2463a68faf72a197
Your Name <you@example.com>
2023-12-05 12:37:53 +0900

    削除ボタンの実装が完了しました。

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const addBtn = document.getElementById("addTodo");
 const input = document.getElementById("input");
 const ul = document.getElementById("todoLIst");
+const doneBtn = document.getElementById("doneTodo");
 
 addBtn.addEventListener("click", addTodo);
+doneBtn.addEventListener("click", doneTodo);
 
 function addTodo() {
   if (input.value == "") {
@@ -15,4 +17,13 @@ function addTodo() {
   ul.appendChild(li);
   li.textContent = input.value;
   input.value = "";
+}
+
+function doneTodo() {
+  const liLength = document.getElementsByTagName("li");
+  if (liLength.length <= 0) {
+    window.alert("削除するもんないよ");
+  }
+  console.log();
+  liLength[liLength.length - 1].remove();
 }
